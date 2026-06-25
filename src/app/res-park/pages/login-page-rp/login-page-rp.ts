b68525a608a3fce2c6e6eda279e4f63152f6b4b5
Edgardo Santander <edgarsa1209@gmail.com>
2026-06-25 11:13:36 -0600

    add login-page with icons and images directory

--- src/app/res-park/pages/login-page-rp/login-page-rp.ts
+++ src/app/res-park/pages/login-page-rp/login-page-rp.ts
@@ -1,8 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-login-page-rp',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    PasswordModule,
+    CheckboxModule,
+    ButtonModule
+  ],
   templateUrl: './login-page-rp.html',
+  styleUrl: './login-page-rp.css',
 })
-export default class LoginPageRp {}
+export default class LoginPageRp {
+  private fb = inject(FormBuilder);
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    remember: [false]
+  });
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    console.log('login form', this.loginForm.getRawValue());
+    // luego aquí conectas el auth real
+  }
+
+  loginWithGoogle(): void {
+    console.log('login google');
+  }
+
+  loginWithMicrosoft(): void {
+    console.log('login microsoft');
+  }
+
+
+}
